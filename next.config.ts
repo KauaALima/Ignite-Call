f64cf6546866b0c3732d1fc9e1fd74123fd024da
Kauã Araujo de Lima <lkauan462@gmail.com>
2024-12-26 22:12:49 +0000

    Eslint ignore builds :tada:

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
