6b6431bc7bc3790fc0c3b836580b045cddc541a6
Kauã Araujo de Lima <lkauan462@gmail.com>
2024-12-27 17:14:46 +0000

    Test build

---
+++ src/utils/BuildAuthOptions.ts
@@ -0,0 +1,56 @@
+import { PrismaAdapter } from '../lib/auth/prisma-adapter'
+import { NextApiRequest, NextApiResponse, NextPageContext } from 'next'
+import NextAuth, { NextAuthOptions } from 'next-auth'
+import GoogleProvider, { GoogleProfile } from 'next-auth/providers/google'
+
+export function BuildAuthOptions(
+    req: NextApiRequest | NextPageContext['req'],
+    res: NextApiResponse | NextPageContext['res'],
+  ): NextAuthOptions {
+    return {
+      adapter: PrismaAdapter(req, res),
+  
+      providers: [
+        GoogleProvider({
+          clientId: process.env.GOOGLE_CLIENT_ID as string,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+          authorization: {
+            params: {
+              prompt: 'consent',
+              access_type: 'offline',
+              response_type: 'code',
+              scope:
+                'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
+            },
+          },
+          profile(profile: GoogleProfile) {
+            return {
+              id: profile.sub,
+              name: profile.name,
+              username: '',
+              email: profile.email,
+              avatar_url: profile.picture,
+            }
+          },
+          allowDangerousEmailAccountLinking: true,
+        }),
+      ],
+      callbacks: {
+        async signIn({ account }) {
+          if (
+            !account?.scope?.includes('https://www.googleapis.com/auth/calendar')
+          ) {
+            return '/register/connect-calendar/?error=permissions'
+          }
+          return true
+        },
+  
+        async session({ session, user }) {
+          return {
+            ...session,
+            user,
+          }
+        },
+      },
+    }
+  }
