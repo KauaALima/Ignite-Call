1770c9387a6492bdbfbac0d41ada86d207a6611a
Kauã Araujo de Lima <lkauan462@gmail.com>
2024-12-27 16:20:11 +0000

    edit session wrapper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Roboto } from 'next/font/google'
 import './globals.css'
-import SessionWrapper from '@/components/SessionWrapper'
+import SessionWrapper from '../components/SessionWrapper'
 
 const roboto = Roboto({
   subsets: ['latin'],
